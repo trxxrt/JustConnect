9d67ef65ed9a2f143d91484b09e2604581940de3
jc <jc@jc-laptop.(none)>
2011-10-30 14:11:11 +0100

    * fix du bug du pseudo du joueur lors de la fin de partie

--- view/game_over.c
+++ view/game_over.c
@@ -3,13 +3,15 @@
 char* display_game_over (gpointer pt)
 {
     // 0. déclaration des variables locales
+    int i = 0;
     GtkWidget* dialog = NULL;
     GtkWidget* content_area = NULL;
     GtkWidget* label = NULL;
     GtkWidget* entry = NULL;
+    GtkWidget* entry_buffer = NULL;
     char* buffer = (char*)malloc(15*sizeof(char));
 
-    // 1. première boite de dialogue de séléction de la difficulté
+    // 1. boite de dialogue de séléction du pseudo
 
         // 1.1 création de la boite
     dialog = gtk_dialog_new();
@@ -27,10 +29,11 @@ char* display_game_over (gpointer pt)
     gtk_box_pack_end (GTK_BOX(content_area), label, TRUE, TRUE, 0);
 
         // 1.4 ajout du gtkentry
-    entry = gtk_entry_new();
+    entry_buffer = gtk_entry_buffer_new("", 0);
+    entry = gtk_entry_new_with_buffer(entry_buffer);
     gtk_entry_set_max_length(GTK_ENTRY(entry), 15);
     gtk_box_pack_end (GTK_BOX(content_area), entry, TRUE, TRUE, 0);
-    g_object_ref(entry);
+    strcpy(buffer, gtk_entry_get_text(GTK_ENTRY(entry)));
 
         // 1.5 ajout des boutons de choix
     gtk_dialog_add_button(GTK_DIALOG(dialog), GTK_STOCK_OK, 1);
@@ -39,9 +42,17 @@ char* display_game_over (gpointer pt)
         // 1.6 activation du widget
     gtk_widget_show_all(dialog);
     gtk_dialog_run (GTK_DIALOG (dialog));
+
+    // 2. recopie de la chaine de caractère
+    strcpy(buffer, gtk_entry_get_text(GTK_ENTRY(entry)));
+    if(DEBUG) printf("pseudo entré par le joueur : %s\n", buffer);
+
+    // 3. destruction des widgets
     gtk_widget_destroy (dialog);
 
-    strcpy(buffer, gtk_entry_get_text(GTK_ENTRY(entry)));
+    // 4. on vérifie qu'il n'y a pas d'espace
+    for(i=0; i<strlen(buffer); i++)
+        if(buffer[i] == ' ') buffer[i] = '_';
 
     return buffer;
 }
